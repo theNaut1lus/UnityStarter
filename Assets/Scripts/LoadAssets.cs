ba78ccf983e666536b042c88e6effdd6e421f39f
Sidak Aulakh <Sidak.S.Aulakh@student.uts.edu.au>
2024-08-26 19:01:37 +1000

    60 percent complete

--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class LoadAssets : MonoBehaviour
 {
-    public GameObject redObj;
+    public GameObject redObj = new GameObject();
     // Start is called before the first frame update
     void Start()
     {
