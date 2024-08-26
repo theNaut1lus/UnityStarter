db2f37f17328ee10e49d67a41565830cdca1d2b3
Sidak Aulakh <Sidak.S.Aulakh@student.uts.edu.au>
2024-08-26 20:17:01 +1000

    100 percent complete

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-public class ConsolePrint : MonoBehaviour
+public class PrintAndHide : MonoBehaviour
 {
     [SerializeField]
     private GameObject gameObj;
@@ -11,16 +14,30 @@ public class ConsolePrint : MonoBehaviour
     public Renderer rend;
 
     private int i;
+    private int j;
+    
     // Start is called before the first frame update
     void Start()
     {
         i = 3;
         gameObj = this.gameObject;
+        if(gameObj.CompareTag("Blue") && i == Random.Range(150,251))
+        {
+            rend.enabled = false;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         Debug.Log(gameObj.name + ":"+ ++i);
+        if(gameObj.CompareTag("Red") && i == 100)
+        {
+            gameObj.SetActive(false);
+        }
+        if(gameObj.CompareTag("Blue") && i is >= 150 and <= 250)
+        {
+            rend.enabled = false;
+        }
     }
 }
